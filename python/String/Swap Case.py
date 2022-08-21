3927ea357fb3d9501b7d3f8c1e29ef2b7c3f73ef
Shafi Usman <shafiusman000@gmail.com>
2022-08-21 20:37:14 +0530

    swap case py addedto new folder String

---
+++ python/String/Swap Case.py
@@ -0,0 +1,14 @@
+def swap_case(s):
+    li = []
+    for i in s:
+        if i.islower():
+           i = i.upper()
+        elif i.isupper():
+           i = i.lower()
+        li.append(i)
+    return "".join(li)
+    
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
